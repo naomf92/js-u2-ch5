b391917ad7744ad5cd4894288473a1cb3c420c6c
naomi <iam.naomi92@gmail.com>
2021-03-12 18:33:25 +0900

    challenge5

--- src/index.js
+++ src/index.js
@@ -8,11 +8,15 @@ class TranslationApp {
 
   /*現在のLocaleに合わせて、polyglotにメッセージをセットします。メッセージのセットにはpolyglot.extend()を利用します。*/
   setup() {
-    // どちらも共通のオブジェクトプロパティ名にしてしまって、if文条件分岐で値だけを入れ替えると、t()メソッド呼び出し記述が一度で済みます & Polyglotの構文上、オブジェクトのプロパティが文字列ですが、変数、定数、関数、引数のような役割をするので、"helo" もしくは"helloEn"などにしておくと、呼び出しの際にオブジェクトのプロパティだと視覚的にわかりやすいです
-    polyglot.extend({
-      "hello world": "こんにちは、世界",
-      "こんにちは、世界":"hello world"
-    });
+    if (this.currentLocale = ja) {
+      polyglot.extend({
+        'hello': 'こんにちは、世界'
+      });
+    } else {
+      polyglot.extend({
+        'hello': 'Hello World'
+      });
+    }
   }
 
   /*ボタンにセットされたdata-localeを元に現在のlocaleを変更します。*/
@@ -22,9 +26,7 @@ class TranslationApp {
   /*mainというidがセットされた要素の下にh1タグで現在のlocaleに応じて、メッセージを表示します。*/
   showMessage() {
     const main = document.getElementById('main');
-    // 翻訳文章データは、polyglot.extend()内にセットなので、英語のデータも、関数setup内で日本語と同じように用意をします
-    // 上記のように修正しましょう
-    main.innerHTML = polyglot.t("hello world");
+    main.innerHTML = polyglot.t('hello');
   }
 
 }
