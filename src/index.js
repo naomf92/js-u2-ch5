e63e8a3f6bf05420ad1520203034875c2a34a06c
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-03-16 22:12:06 +0100

    fix: add advice for reusability in func updateLocale

--- src/index.js
+++ src/index.js
@@ -26,7 +26,9 @@ class TranslationApp {
     //console.log(e.target.dataset.locale);
     const locale = e.target.dataset.locale;
 
+    // 日本語の時、英語の時と条件分岐をしなくても、「その時に選ばれたlocaledata属性の文字列取得がされた時」としておくと、どれだけ言語が増えても、関数updateLocaleはリファクタリングをしなくてもよくなります。（再利用性の高さ）
     if(locale === 'ja') {
+      // this.currentLocaleに代入している値、'ja'は、定数localeでもありますね
       this.currentLocale = 'ja';
       console.log(this.currentLocale);
     }
