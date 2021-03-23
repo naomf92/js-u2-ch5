0d078e1660131d017974009f402d6299ae41cd11
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-03-23 20:13:00 +0100

    fix: add advice for the value this showed

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ class TranslationApp {
   constructor() {
     this.polyglot = new Polyglot();
     this.currentLocale = localStorage.getItem('locale') || 'ja';//データの取得
+    // ここに本来thisに参照して欲しい関数を、bindメソッドでthisが思うような値を参照してくれない挙動をbind（固定）してしまい、スコープを超えた関数がthisを使ってきちんと取得できるように調整します。
+    // 3つの関数全てが関わっている関数がbindされると、thisの挙動を1つの記述だけで押さえられるかもしれません
   }
 
   /*現在のLocaleに合わせて、polyglotにメッセージをセットします。メッセージのセットにはpolyglot.extend()を利用します。*/
@@ -26,6 +28,8 @@ class TranslationApp {
     const locale = e.target.dataset.locale;
     this.currentLocale = locale;
     localStorage.setItem('locale', locale);//データの保存
+    // ↓ 関数setupなどに付与されているthisにどんな値が格納されているか検証すると、クリックしたボタン要素が取得されてしまいます。本来thisが参照すべき値を参照していないかもしれません。
+    console.log(this);
     this.setup();
     this.showMessage();
   }
@@ -41,6 +45,8 @@ class TranslationApp {
 const app = new TranslationApp();
 
 {
+  // ↓ TranslationAppクラスに格納されている値が関数を含め、全て表示されます。関数部分をどんどん開いていくと、「thisの参照する値を固定するメソッド、bind」が見つかります
+  console.log(app);
   const button1 = document.getElementById('button1');
   button1.addEventListener("click", app.updateLocale);
 
