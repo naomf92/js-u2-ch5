a0bac4e0bdc893709fc7eb33f116a5da66457b0b
naomi <iam.naomi92@gmail.com>
2021-03-23 21:38:08 +0900

    challenge5

--- src/index.js
+++ src/index.js
@@ -3,25 +3,18 @@ import Polyglot from 'node-polyglot';
 class TranslationApp {
   constructor() {
     this.polyglot = new Polyglot();
-    this.currentLocale = localStorage.getItem('locale');//データの取得 → ここに1つにまとめてORの演算子で書くことができます
-
-    //データながければデフォルトでja → ORの演算子を使うと1行で「変数 = 値;」の形ですっきり書けます
-    if (locale === ない) {
-      this.currentLocale = 'ja';
-    }
-
-    console.log(this.currentLocale);
+    this.currentLocale = localStorage.getItem('locale') || 'ja';//データの取得
   }
 
   /*現在のLocaleに合わせて、polyglotにメッセージをセットします。メッセージのセットにはpolyglot.extend()を利用します。*/
   setup() {
     if (this.currentLocale === 'ja') {
-      polyglot.extend({
+      this.polyglot.extend({
         'hello': 'こんにちは、世界'
       });
     }
     if(this.currentLocale === 'en') {
-      polyglot.extend({
+      this.polyglot.extend({
         'hello': 'Hello World'
       });
     }
@@ -33,12 +26,14 @@ class TranslationApp {
     const locale = e.target.dataset.locale;
     this.currentLocale = locale;
     localStorage.setItem('locale', locale);//データの保存
+    this.setup();
+    this.showMessage();
   }
 
   /*mainというidがセットされた要素の下にh1タグで現在のlocaleに応じて、メッセージを表示します。*/
   showMessage() {
     const main = document.getElementById('main');
-    main.innerHTML = polyglot.t('hello');
+    main.innerHTML = this.polyglot.t('hello');
   }
 
 }
@@ -51,4 +46,7 @@ const app = new TranslationApp();
 
   const button2 = document.getElementById('button2');
   button2.addEventListener("click", app.updateLocale);
+
+  app.setup();
+  app.showMessage();
 }
